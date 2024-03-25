8fbf3335f134451fcb2fbe506eef903684e88359
JiHye Yoon <yoonjihyeoi@gmail.com>
2024-03-25 12:19:23 -0700

    user update

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const bcrypt = require("bcrypt");
 const db = require("../db");
 
 const { SECRET_KEY, BCRYPT_WORK_FACTOR } = require("../config");
+const { BadRequestError, NotFoundError } = require("../expressError");
 
 /** User of the site. */
 
@@ -55,12 +56,30 @@ class User {
   /** Update last_login_at for user */
 
   static async updateLoginTimestamp(username) {
+
+    const result = await db.query(
+      `UPDATE users
+        SET last_login_at = current_timestamp
+        WHERE username = $1
+        RETURNING username, last_login_at`, [username]);
+
+    const user = result.rows[0];
+
+    if (!user) throw new NotFoundError(`No such message: ${username}`);
+
+    return user;
   }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name}, ...] */
 
   static async all() {
+    const result = await db.query(
+      `SELECT username, first_name, last_name
+        FROM users
+        ORDER BY username`);
+
+    return result.rows;
   }
 
   /** Get: get user by username
@@ -73,6 +92,15 @@ class User {
    *          last_login_at } */
 
   static async get(username) {
+    const result = db.query(
+      `SELECT username, first_name, last_name, phone, join_at, last_login_at
+        FROM users
+        WHERE username = $1`, [username]);
+
+    const user = result.rows[0];
+
+    if (!user) throw new NotFoundError(`No such user: ${username}`)
+    return user;
   }
 
   /** Return messages from this user.
@@ -84,6 +112,19 @@ class User {
    */
 
   static async messagesFrom(username) {
+
+    // user info
+    const mResults = await db.query(
+      `SELECT m.id,
+        (SELECT u.username, u.first_name, u.last_name, u.phone
+          FROM users AS u
+          WHERE u.username = m.to_username), m.body, m.sent_at, m.read_at
+        FROM messages AS m
+        WHERE m.from_username = $1`, [username]
+    );
+
+    const messages = mResults.rows;
+    return res.json();
   }
 
   /** Return messages to this user.
@@ -95,6 +136,25 @@ class User {
    */
 
   static async messagesTo(username) {
+    const uResults = await db.query(
+      `SELECT username, first_name, last_name, phone
+        FROM users
+        WHERE username = $1`, [username]
+    );
+
+    const user = uResults.rows[0];
+
+    const mResults = await db.query(
+      `SELECT id, from_user, body, sent_at, read_at
+        FROM message
+        WHERE to_username = $1`, [username]
+    );
+
+    const messages = mResults.rows;
+
+    return messages
+
+
   }
 }
 
